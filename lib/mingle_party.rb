f31ee4173a53a824aa1ce1d69def9318b71ffeed
Sudhindra Rao [betarelease] <sudhindra.r.rao@gmail.com>
2012-10-08 09:21:24 -0700

    cleanup common code; ignore config.yml

--- lib/mingle_party.rb
+++ lib/mingle_party.rb
@@ -6,23 +6,22 @@ class MingleParty
 
   def initialize(host, username, password, project)
     @uri = "#{host}/api/v2/projects/#{project}"
-    @auth = {:username => username, :password => password}
+    @auth = { :username => username, :password => password }
+    @auth_options = { :basic_auth => @auth }
   end
 
   def get(number)    
-    response = self.class.get("#{@uri}/cards/#{number}.xml", :basic_auth => @auth)
-    Crack::XML.parse(response.body)
+    response = self.class.get( "#{@uri}/cards/#{number}.xml", @auth_options )
+    Crack::XML.parse( response.body )
   end
   
   def put(number, status)
-    options = { :query => { 'card[properties[][name]' => 'status', 'card[properties][][value]' => status }, 
-                :basic_auth => @auth }
+    options = @auth_options.merge { :query => { 'card[properties[][name]' => 'status', 'card[properties][][value]' => status } }
     response  = self.class.put( "#{@uri}/cards/#{number}.xml", options )
   end
   
   def post(message, command)
-    options = { :query => { command => message }, 
-                :basic_auth => @auth }
+    options = @auth_options.merge { :query => { command => message } } 
     response  = self.class.post( "#{@uri}/murmurs.xml", options )
   end
 end
--- spec/mingle_party_spec.rb
+++ spec/mingle_party_spec.rb
@@ -4,10 +4,14 @@ describe MingleParty do
   
   before :all do
     @config = YAML.load(File.read("./config.yml")).first
+    @host = @config['host']
+    @username = @config['username']
+    @password = @config['password']
+    @project =  @config['project']
   end
   
   it "should find card" do
-    mingle = MingleParty.new(@config['host'], @config['username'], @config['password'], @config['project'])
+    mingle = MingleParty.new(host, username, password, project)
     card = mingle.get(13)
 
     card['card']['properties'].first['value'].should =~ /done/
@@ -16,7 +20,7 @@ describe MingleParty do
   end
 
   it "should change card status to done" do
-    mingle = MingleParty.new(@config['host'], @config['username'], @config['password'], @config['project'])
+    mingle = MingleParty.new(host, username, password, project)
     card = mingle.put(11, "done")
 
     card['card']['name'].should =~ /card 11/
@@ -24,14 +28,14 @@ describe MingleParty do
   end
 
   it "should murmur about its happiness" do
-    mingle = MingleParty.new(@config['host'], @config['username'], @config['password'], @config['project'])
+    mingle = MingleParty.new(host, username, password, project)
     murmur = mingle.post('happy to mingle xoxo siri', 'murmur[body]')
 
     murmur['murmur']['body'].should =~ /happy to mingle/
   end
   
   it "should accept a command for post" do
-    mingle = MingleParty.new(@config['host'], @config['username'], @config['password'], @config['project'])
+    mingle = MingleParty.new(host, username, password, project)
     post_with_command = mingle.post('happy to mingle xoxo siri', 'card[new]')
 
     post_with_command.should be_true
