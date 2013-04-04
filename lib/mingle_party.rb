7122bf402f65b234ce37e694c61770c91f8d0a76
RyanDurling <ryan.p.durling@gmail.com>
2013-04-04 10:16:15 -0700

    add fetch all cards avility

--- lib/mingle_party.rb
+++ lib/mingle_party.rb
@@ -1,5 +1,6 @@
 require 'httparty'
 require 'crack'
+require 'rexml/document'
 
 class MingleParty
   include HTTParty
@@ -10,7 +11,7 @@ class MingleParty
     auth = { username: config['username'], password: config['password'] }
     @auth_options = { :basic_auth => auth }
   end
-  
+
   def create_card(name, card_type)
     options = @auth_options.merge({ query: {'card[name]' =>  name, 'card[card_type_name]' => card_type } })
     response = self.class.post("#{@uri}/cards.xml", options)
@@ -21,31 +22,46 @@ class MingleParty
     response = self.class.put( "#{@uri}/cards/#{number}.xml", options )
   end
 
-  def fetch_card(number)    
+  def fetch_card(number)
     response = self.class.get( "#{@uri}/cards/#{number}.xml", @auth_options )
     Crack::XML.parse( response.body )
   end
+
+  def fetch_cards
+    response = self.class.get( "#{@uri}/cards.xml", @auth_options )
+    Crack::XML.parse( response.body )
+  end
+
+=begin
+  def delete_card(number)
+    self.class.delete( "#{@uri}/cards/#{number}")
+  end
+
+  def delete_cards
+    response = fetch_cards
+    response['cards'].each{|card| delete_card(card['number'].to_i)}
+    response.each {|key, value| puts "#{key}" }
+  end
+=end
 
   def murmur(message, command)
     options = @auth_options.merge( { query:  { "#{command}".to_sym =>  message } } )
     response = self.class.post( "#{@uri}/murmurs.xml", options )
   end
-  
+
   def create_user(user)
-    options = @auth_options.merge({ query: { 'user[name]' =>  user[:name], 'user[login]' => user[:login], 
-                                             'user[email]' => user[:email], 'user[admin]' => user[:admin],
-                                             'user[password]' => user[:password], 
-                                             'user[password_confirmation]' => user[:password_confirmation]} })                                      
+    options = @auth_options.merge({ query: { 'user[name]' =>  user[:name], 'user[login]' => user[:login],
+                                      'user[email]' => user[:email], 'user[admin]' => user[:admin],
+                                      'user[password]' => user[:password],
+                                      'user[password_confirmation]' => user[:password_confirmation]} })
     self.class.post("#{@uri}/users.xml", options)
   end
-  
+
   def fetch_users
     response = self.class.get( "#{@uri}/users.xml", @auth_options )
-    puts "[DEBUG] response => #{response.inspect}"
-    
     Crack::XML.parse( response.body )
   end
-  
+
   def fetch_user(id)
     response = self.class.get( "#{@uri}/users/#{id}.xml", @auth_options )
     Crack::XML.parse( response.body )
--- spec/mingle_party_spec.rb
+++ spec/mingle_party_spec.rb
@@ -1,21 +1,20 @@
 require 'spec_helper'
 
 describe MingleParty do
-  
-  it "find card" do
-    mingle = MingleParty.new
-    response = mingle.create_card('card status change test', 'card')
-    puts "[DEBUG] response => #{response.inspect}"
-    
-    card = mingle.fetch_card(13)
+  let(:mingle) {MingleParty.new}
 
+  it "find card" do
+    response = mingle.create_card('card status change test', 'card')
+
+    card = mingle.fetch_cards
+
+    card.should include_json('Just started')
     card['card']['properties'].first['value'].should =~ /Just started/
     card['card']['name'].should =~ /card 13/
     card['card']['description'].should == "I love Siri and So should you."
   end
 
   it "change card status to done" do
-    mingle = MingleParty.new
     response = mingle.create_card('card status change test', 'card')
     card_number = response['location'].split("/").last.split(".").first
     card = mingle.fetch_card(card_number)
@@ -27,52 +26,50 @@ describe MingleParty do
   end
 
   it "murmur about its happiness" do
-    mingle = MingleParty.new
     murmur = mingle.murmur('happy to mingle xoxo siri', 'murmur[body]')
 
     murmur['murmur']['body'].should =~ /happy to mingle/
   end
-  
+
   it "create a card" do
-    mingle = MingleParty.new
     response = mingle.create_card('black jack', 'card')
     card_number = response['location'].split("/").last.split(".").first
     card = mingle.fetch_card(card_number)
-    
-    card.should_not be_nil    
+
+    card.should_not be_nil
     card['card']['name'].should =~ /black jack/
   end
-  
+
   it "create an admin user" do
-    mingle = MingleParty.new
-    user = { :name => "jsmith", 
-             :login => "johnsmithcom", 
-             :email => "john@smith.com", 
-             :password => "abcd1234", 
-             :password_confirmation => "abcd1234", 
+    user = { :name => "jsmith",
+             :login => "johnsmithcom",
+             :email => "john@smith.com",
+             :password => "abcd1234",
+             :password_confirmation => "abcd1234",
              :admin => "false" }
     response = mingle.create_user(user)
     response.should be_success
   end
 
   it "create a system user" do
-    mingle = MingleParty.new
-    user = { :name => "jsystem", 
-             :login => "john@system.com", 
-             :email => "john@system.com", 
-             :password => "abcd1234", 
-             :password_confirmation => "abcd1234", 
-             :admin => "false", 
+    user = { :name => "jsystem",
+             :login => "john@system.com",
+             :email => "john@system.com",
+             :password => "abcd1234",
+             :password_confirmation => "abcd1234",
+             :admin => "false",
              :system => "true" }
     response = mingle.create_user(user)
     response.should be_success
   end
 
   it "fetch users" do
-    mingle = MingleParty.new
-
     response = mingle.fetch_users
     users = response['projects_members']
     users.should_not be_empty
+  end
+
+  it "fetches all the cards" do
+
   end
 end
